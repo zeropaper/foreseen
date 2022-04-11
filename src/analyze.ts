4c0f91337741c646293ed3d926328f6f1c556d9c
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-04-11 14:55:08 +0200

    fix(analyze): allow expressions like 12 -13

--- src/analyze.ts
+++ src/analyze.ts
@@ -1,10 +1,14 @@
 export type Sc = (string | number | Sc)[];
 
-const expReg = /(([a-z]*)\([^\)]+\)|-[0-9]+|[^\s+-]+|[+-]{1,1})/ig;
+const expReg = /(([a-z]*)\([^\)]+\)|[-]{0,1}[0-9.]+|[^0-9\s]{1,1}|[^\s]+)/ig;
 
 export const analyze = (str: string): Sc => {
-  const matches = str.match(expReg);
-  return (matches ? matches.map(match => {
+  let matches = str.match(expReg);
+  if (!matches) return [];
+  if (matches.length === 2 && matches[1].startsWith('-')) {
+    matches = [matches[0], '-', matches[1].slice(1)];
+  }
+  return matches.map(match => {
     if (!Number.isNaN(Number(match)))
       return Number(match);
 
