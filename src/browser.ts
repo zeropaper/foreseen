308169340924398884d6ae32ff078864aa5443fd
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-04-21 17:09:28 +0200

    chore: adapt browsere dev environment

--- src/browser.ts
+++ src/browser.ts
@@ -21,29 +21,49 @@ if (typeof window !== 'undefined' && typeof window.Foreseen === 'undefined') {
 // @ts-ignore
 const input = window.input || document.querySelector('#input')
 // @ts-ignore
-const container = window.container || document.querySelector('#container')
+const canvasContainer = window.canvasContainer || document.querySelector('#canvasContainer')
+// @ts-ignore
+const editorContainer = window.editorContainer || document.querySelector('#editorContainer')
+// @ts-ignore
+const controlsContainer = window.controlsContainer || document.querySelector('#controlsContainer')
+// @ts-ignore
+const debugContainer = window.debugContainer || document.querySelector('#debugContainer')
 
-const instance = new Foreseen(input.value);
-container.appendChild(instance.defaultRenderer.domElement)
-instance.render();
+const instance = new Foreseen(window.THREE, input.value);
+canvasContainer.appendChild(instance.domElement)
+
+setInterval(() => {
+  debugContainer.textContent = JSON.stringify(instance.data, null, 2)
+}, 1000)
+
+// @ts-ignore
+window.scene = instance.scene;
+// @ts-ignore
+window.meshes = instance.meshes;
+// @ts-ignore
+window.instance = instance;
 
 let editor: monaco.editor.IStandaloneCodeEditor | undefined;
 
 const handleChange = () => {
-  console.info('change', validate(input.value))
-  instance.update(input.value)
+  // console.info('change', validate(input.value))
+  instance.update(input.value).render()
 }
 
 const handleResize = () => {
-  instance.defaultRenderer.setSize(container.clientWidth, container.clientHeight)
+  console.info('handleResize %s/%s', canvasContainer.clientWidth, canvasContainer.clientHeight)
+  instance.defaultRenderer.setSize(canvasContainer.clientWidth, canvasContainer.clientHeight)
   if (instance.defaultCamera.type === 'PerspectiveCamera') {
-    instance.defaultCamera.aspect = container.clientWidth / container.clientHeight
+    instance.defaultCamera.aspect = canvasContainer.clientWidth / canvasContainer.clientHeight
     instance.defaultCamera?.updateProjectionMatrix()
   }
+  instance.render()
+  editor.layout(editorContainer)
 }
 
 window.addEventListener('load', () => {
-  editor = monaco.editor.create(container, {
+  editor = monaco.editor.create(editorContainer, {
+    automaticLayout: true,
     value: input.value,
     language: 'yaml'
   });
@@ -54,6 +74,38 @@ window.addEventListener('load', () => {
   input.style.display = 'none'
   handleChange()
   handleResize()
+  instance.render()
+  // instance.startRenderLoop().clock.start();
 })
 window.addEventListener('resize', handleResize)
 input.addEventListener('keyup', handleChange)
+
+const renderingButton = document.createElement('button');
+renderingButton.textContent = `rendering: ${instance.data.isRendering}`;
+renderingButton.addEventListener('click', () => {
+  if (instance.data.isRendering) {
+    instance.stopRenderLoop()
+  } else {
+    instance.startRenderLoop();
+  }
+  renderingButton.textContent = `rendering: ${instance.data.isRendering}`;
+})
+const clockButton = document.createElement('button');
+clockButton.textContent = `clock: ${instance.clock.running}`;
+clockButton.addEventListener('click', () => {
+  if (instance.clock.running) {
+    instance.clock.stop();
+  } else {
+    instance.clock.start();
+  }
+  clockButton.textContent = `clock: ${instance.clock.running}`;
+});
+controlsContainer.append(renderingButton)
+controlsContainer.append(clockButton)
+
+// if (module.hot) {
+//   module.hot.accept('./index.js', function () {
+//     console.log('Accepting the updated printMe module!');
+//     printMe();
+//   })
+// }
