5623b5fc273fec22051cf756f0e75e0b078ce793
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-04-28 09:49:11 +0200

    build: attempt to publish to gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,53 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MonacoEditorWebpackPlugin = require('monaco-editor-webpack-plugin');
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    port: 9000,
-    static: {
-      directory: __dirname,
+const path = require('path');
+
+module.exports = (env, argv) => {
+  let config = {
+    mode: argv.mode || 'production',
+    entry: './src/browser.ts',
+    output: {
+      path: path.resolve(__dirname, 'docs'),
+      filename: 'browser.js',
     },
-  },
-  entry: './src/browser.ts',
-  output: {
-    filename: 'browser.js',
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, loader: 'ts-loader', exclude: /test/ },
-      // for monaco editor
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.ttf$/,
-        type: 'asset/resource',
-      },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        { test: /\.ts$/, loader: 'ts-loader', exclude: /test/ },
+        // for monaco editor
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.ttf$/,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'dev.html',
+      }),
+      new MonacoEditorWebpackPlugin({
+        languages: ['yaml'],
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'dev.html',
-    }),
-    new MonacoEditorWebpackPlugin({
-      languages: ['yaml'],
-    }),
-  ],
+  };
+
+  if (argv.mode === 'development') {
+    config = {
+      ...config,
+      devtool: 'inline-source-map',
+      devServer: {
+        port: 9000,
+        static: {
+          directory: __dirname,
+        },
+      },
+    };
+  }
+  return config;
 };
