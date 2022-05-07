86087f5e4d75ead0e38cf7947fcc9b46bc3396b3
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-05-07 11:59:01 +0200

    chore(materials): add shadow type arguments

--- src/materialArguments.ts
+++ src/materialArguments.ts
@@ -2,5 +2,6 @@ export const materialArguments = {
   basic: ['color', 'map', 'lightMap', 'specularMap', 'envMap', 'combine', 'reflectivity', 'refractionRatio', 'fog'],
   lambert: ['color', 'map', 'lightMap', 'specularMap', 'envMap', 'combine', 'reflectivity', 'refractionRatio', 'fog'],
   phong: ['color', 'map', 'lightMap', 'specularMap', 'envMap', 'combine', 'reflectivity', 'refractionRatio', 'shininess', 'fog'],
+  shadow: ['color', 'fog', 'transparent'],
   standard: ['color', 'roughness', 'metalness', 'map', 'lightMap', 'specularMap', 'envMap', 'combine', 'reflectivity', 'refractionRatio', 'fog'],
 };
