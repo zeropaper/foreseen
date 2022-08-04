9950d293dabac9769cdbc1b065300a71f5d645fc
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-08-04 20:00:10 +0200

    feat(special): add base for special object

---
+++ test/special.test.ts
@@ -0,0 +1,18 @@
+import Foreseen from '../src';
+import freshInstance from './freshInstance';
+
+let instance: Foreseen;
+
+beforeEach(() => {
+  console.info('trakak');
+  instance = freshInstance();
+});
+
+describe('special', () => {
+  it('does things', () => {
+
+  });
+  it('does things again', () => {
+
+  });
+});
